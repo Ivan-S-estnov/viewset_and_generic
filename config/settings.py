490f0356b99d8daf5449488ecfc0c4c0adfbc6de
Ivan <jaloylee@yandex.ru>
2025-06-30 11:10:10 +0400

    fixed

--- config/settings.py
+++ config/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework_simplejwt",
     "rest_framework",
-    "drf_yasg",
     "django_filters",
     "drf_spectacular",
     "users",
